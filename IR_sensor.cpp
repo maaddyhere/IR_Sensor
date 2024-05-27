d181975a69c54c8ab4f6c44c8e29915a63f24518
Muqadas <muqaddasamjadpgc11@gmail.com>
2024-05-27 09:14:04 +0500

    Add files via upload

--- IR_sensor.cpp
+++ IR_sensor.cpp
@@ -1,30 +1,29 @@
-int Redled=8;
-int Greenled=9;
-int sensor=7;
+int ledpin=13;
+int IrSensor=12;
 int value;
 
+
 void setup() {
-  pinMode(Redled,OUTPUT);
-  pinMode(Greenled,OUTPUT);
-  pinMode(sensor,INPUT);
+  pinMode(IrSensor,INPUT);
+  pinMode(ledpin,OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  value=digitalRead(sensor);
+  value=digitalRead(IrSensor);
   if(value==0)
   {
-    digitalWrite(Greenled,HIGH);
-    delay(1000);
-    digitalWrite(Greenled,LOW);
     Serial.println(value);
+    digitalWrite(ledpin,HIGH);
+    delay(1000);
+
   }
   else
   {
-    digitalWrite(Redled,HIGH);
-    delay(1000);
-    digitalWrite(Redled,LOW);
     Serial.println(value);
+    digitalWrite(ledpin,LOW);
+    delay(1000);
   }
+  
 
 }
